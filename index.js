38ceb8753cbc420b59196faa88dfd20cf38eca2c
Perezented <57721734+Perezented@users.noreply.github.com>
2020-01-27 16:08:16 -0600

    I think I did that first question wrong.Might be right now.

--- index.js
+++ index.js
@@ -38,14 +38,14 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
  */
-function makePersonObject(/* code here */) {
+function makePersonObject(a, b, c) {
     /* code here */
-    let newLeia = {
+    let Leia = {
         id: 5,
         name: "Leia",
         email: "Leia@Leia.com"
     };
-    return newLeia;
+    return Leia;
 }
 
 /**
@@ -61,8 +61,9 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
  */
-function getName(/* code here */) {
+function getName() {
     /* code here */
+    return "Hello, my name is " + name + ".";
 }
 
 /**
